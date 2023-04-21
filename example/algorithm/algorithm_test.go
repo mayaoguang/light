76601d3edaff1211d99753c8a4338189a7940496
yuanhao <yuanhao@krquant.com>
2023-04-21 12:12:38 +0800

    tree

--- example/algorithm/algorithm_test.go
+++ example/algorithm/algorithm_test.go
@@ -3,6 +3,7 @@ package algorithm
 import (
 	"fmt"
 	"light/example/algorithm/dp"
+	"light/example/algorithm/tree"
 	"testing"
 )
 
@@ -12,8 +13,29 @@ func TestFibonacci(t *testing.T) {
 }
 
 func TestLengthOfLIS(t *testing.T) {
-	fmt.Println(dp.LengthOfLIS([]int{10, 9, 2, 5, 3, 7, 101, 18}))
+	fmt.Println(dp.LengthOfASC([]int{10, 9, 2, 5, 3, 7, 101, 18}))
 }
 func TestCommonSub(t *testing.T) {
 	fmt.Println(dp.LongestCommonSubsequence("abc", "cdeabddc"))
+}
+
+func TestTree(t *testing.T) {
+	a := tree.NewBinTreeNode(1)
+	tree1 := tree.NewBinaryTree(a)
+	a.SetLChild(tree.NewBinTreeNode(2))
+	a.SetRChild(tree.NewBinTreeNode(5))
+	a.GetLChild().SetRChild(tree.NewBinTreeNode(3))
+	a.GetLChild().GetRChild().SetLChild(tree.NewBinTreeNode(4))
+	a.GetRChild().SetLChild(tree.NewBinTreeNode(6))
+	a.GetRChild().SetRChild(tree.NewBinTreeNode(7))
+	a.GetRChild().GetLChild().SetRChild(tree.NewBinTreeNode(9))
+	a.GetRChild().GetRChild().SetRChild(tree.NewBinTreeNode(8))
+
+	l := tree1.InOrder() //中序遍历
+	for e := l.Front(); e != nil; e = e.Next() {
+		obj, _ := e.Value.(*tree.BinTreeNode)
+		fmt.Printf("data:%v\n", obj.GetData())
+	}
+	result := tree1.Find(6)
+	fmt.Println(result)
 }
